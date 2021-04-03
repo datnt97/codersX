d6c53be5d048759331ae8da6613b940b1bc81bef
SYSTMT\tfs_datnt <datnt3009@gmail.com>
2021-04-03 23:44:16 +0700

    Excercise 11

--- Javascript/excercise.js
+++ Javascript/excercise.js
@@ -101,6 +101,39 @@
 // // Test 2: Expect: 130
 // nearestTo100(10, 130);
 
+/* Excercise 11 */
+/* Viết hàm checkInt để kiểm tra hai số nguyên được nhập vào. 
+  Nếu có 1 số âm và một số dương thì trả về true. 
+  Các trường hợp khác trả về false. 
+  Số 0 là số không âm cũng không dương
+  Tham số:
+  - number1: số nguyên thứ nhất
+  - number2: số nguyên thứ hai
+*/
 
+function checkInt(number1, number2) {
+  // viết code ở đây.	
+  if (number1 == 0 || number2 == 0) {
+    return false;
+  }
+  let result = number1 < 0 && number2 > 0 ||
+    number1 > 0 && number2 < 0 ?
+    true :
+    false;
 
+  console.log(result);
+  return result;
+}
+
+// Test 1 - Expect: true
+checkInt(-1, 2);
+
+// Test 2 - Expect: false
+checkInt(2, 6);
+
+// Test 3 - Expect: false
+checkInt(-2, -6);
+
+// Test 4 - Expect: true
+checkInt(2, -6);
 
